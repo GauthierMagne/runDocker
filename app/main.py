d242d780ddf31f0d3c46632d9b3ec32f341c892d
GauthierMagne <47626683+GauthierMagne@users.noreply.github.com>
2020-07-29 18:32:11 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 from flask import Flask
 import os
 import sys
-NAME=os.getenv("NAME")
-PORT=int(os.getenv("PORT"))
 #ENV=os.getenv("ENV")
 app=Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return f"hello {NAME} test {PORT}"
+    return f"hello test 4000"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=PORT, debug=False)
+    app.run(host="0.0.0.0",port=4000, debug=False)
